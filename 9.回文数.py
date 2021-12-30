33dbe09aa902a83601e0052e5261fac4ff6cd83f
David <david_duan@163.com>
2021-12-30 23:28:05 +0800

    回文数

---
+++ 9.回文数.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=9 lang=python
+#
+# [9] 回文数
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        #转为字符串
+        return str(x) == str(x)[::-1]
+# @lc code=end
+
